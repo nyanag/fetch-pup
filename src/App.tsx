a187d7fc3cf7243eae45e187fd2e8d09ac0f2bf2
Ananya <ananyaganesh01@gmail.com>
2023-05-29 11:05:58 -0400

    Implemented login and routes to Search page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LoginForm from './login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchPage from './search';
 
-function App() {
+const LoginPage: React.FC = () => {
+  const apiUrl = 'https://frontend-take-home-service.fetch.com/auth/login'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element = {<LoginForm apiUrl={apiUrl} />} />
+        <Route path="/search" element = {<SearchPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
-export default App;
+export default LoginPage;
